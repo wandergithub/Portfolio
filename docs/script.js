898f33da0386d965c6e57a755b9d876706d3d299
wandergithub <wandergonzalez0@hotmail.com>
2022-04-05 13:06:37 -0400

    Style menu bar

--- docs/script.js
+++ docs/script.js
@@ -1,10 +1,27 @@
 const btn = document.querySelector('#hamburguer');
 const menuVar = document.querySelectorAll('.menu-var, #nav-bar');
-const content = document.body.querySelectorAll('*:not(.menu-var *)');
+const content = document.body.querySelectorAll('*:not(.menu-var *,#menu-v)');
+const quitbtn = document.querySelector('#quitbtn');
+const menuLinks = document.querySelectorAll('div a');
+const navVar = document.getElementById('nav-bar');
 
-function display() {
+function displayMenu() {
   btn.classList.add('hidden');
   content.forEach((e) => e.classList.add('hidden'));
   menuVar.forEach((e) => e.classList.remove('hidden'));
+  navVar.style.justifyContent = "center";
+  navVar.style.textAlign = "end";
+  document.querySelectorAll('.menu-flex').forEach((e) => e.style.gap = "200px" );
 }
-btn.addEventListener('click', display);
+
+function quitMenu() {
+  quitbtn.classList.add('hidden');
+  menuVar.forEach((e) => e.classList.add('hidden'));
+  content.forEach((e) => e.classList.remove('hidden'));
+  navVar.removeAttribute("style");
+  document.querySelectorAll('.menu-flex').forEach((e) => e.removeAttribute(style));
+}
+
+btn.addEventListener('click', displayMenu);
+quitbtn.addEventListener('click', quitMenu);
+menuLinks.forEach((e) => e.addEventListener('click', quitMenu));
