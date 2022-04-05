563b3d6eb3a9194a626ec818e6a39532268dd7cd
wandergithub <wandergonzalez0@hotmail.com>
2022-04-05 13:53:10 -0400

    solve linter errors

--- docs/script.js
+++ docs/script.js
@@ -2,24 +2,24 @@ const btn = document.querySelector('#hamburguer');
 const menuVar = document.querySelectorAll('.menu-var, #nav-bar');
 const content = document.body.querySelectorAll('*:not(.menu-var *,#menu-v)');
 const quitbtn = document.querySelector('#quitbtn');
-const menuLinks = document.querySelectorAll('div a');
+const menuLinks = document.querySelectorAll('div .menu-flex a ');
 const navVar = document.getElementById('nav-bar');
-
+const aTags = document.querySelectorAll('.menu-flex');
 function displayMenu() {
   btn.classList.add('hidden');
   content.forEach((e) => e.classList.add('hidden'));
   menuVar.forEach((e) => e.classList.remove('hidden'));
-  navVar.style.justifyContent = "center";
-  navVar.style.textAlign = "end";
-  document.querySelectorAll('.menu-flex').forEach((e) => e.style.gap = "200px" );
+  navVar.style.justifyContent = 'center';
+  navVar.style.textAlign = 'end';
+  document.querySelectorAll('.menu-flex').forEach((e) => { e.style.gap = '200px'; });
 }
 
 function quitMenu() {
   quitbtn.classList.add('hidden');
   menuVar.forEach((e) => e.classList.add('hidden'));
   content.forEach((e) => e.classList.remove('hidden'));
-  navVar.removeAttribute("style");
-  document.querySelectorAll('.menu-flex').forEach((e) => e.removeAttribute(style));
+  navVar.removeAttribute('style');
+  aTags.forEach((e) => e.removeAttribute('style'));
 }
 
 btn.addEventListener('click', displayMenu);
