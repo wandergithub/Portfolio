5f8382dcd51cfa737966449ea2545e150544bd41
wandergithub <wandergonzalez0@hotmail.com>
2022-04-07 22:00:23 -0400

    set lowerCase custom error mesage

--- docs/validateForm.js
+++ docs/validateForm.js
@@ -2,12 +2,15 @@ const form = document.querySelector('form');
 const email = document.getElementById('email');
 
 form.addEventListener('submit', (event) => {
-  if (email.value == email.value.toLowerCase()) {
-    form.submit();
-        console.log ('works!!!')
-} else {
+  if (!(email.value == email.value.toLowerCase())) {
     event.preventDefault();
-    //mesage
+    email.setCustomValidity('Lower case use required');
+} else {   
+    // form.submit();
+    console.log ('works!!!')
 }
+});
 
+email.addEventListener('change',function (event) {
+  email.setCustomValidity('');
 });
