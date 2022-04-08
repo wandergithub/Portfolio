58e9ed8055726e508cf946c75651fd318bfaa09a
wandergithub <wandergonzalez0@hotmail.com>
2022-04-08 18:31:50 -0400

    solve linter errors

--- docs/preserveData.js
+++ docs/preserveData.js
@@ -1,43 +1,27 @@
 const inputs = document.querySelectorAll('.input-box');
 
 function storageAvailable(type) {
-  var storage;
+  let storage;
   try {
-      storage = window[type];
-      var x = '__storage_test__';
-      storage.setItem(x, x);
-      storage.removeItem(x);
-      return true;
-  }
-  catch(e) {
-      return e instanceof DOMException && (
-          // everything except Firefox
-          e.code === 22 ||
+    storage = window[type];
+    const x = '__storage_test__';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return e instanceof DOMException && (
+    // everything except Firefox
+      e.code === 22
           // Firefox
-          e.code === 1014 ||
+          || e.code === 1014
           // test name field too, because code might not be present
           // everything except Firefox
-          e.name === 'QuotaExceededError' ||
+          || e.name === 'QuotaExceededError'
           // Firefox
-          e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+          || e.name === 'NS_ERROR_DOM_QUOTA_REACHED')
           // acknowledge QuotaExceededError only if there's something already stored
-          (storage && storage.length !== 0);
+          && (storage && storage.length !== 0);
   }
-}
-
-function loadUserData() {
-  if (storageAvailable('localStorage')) { // Check if storage is abailable
-    if(localStorage.length === 0) {
-      saveUserData();
-    }else{
-      inputs[0].value = localStorage.getItem('name');
-      inputs[1].value = localStorage.getItem('email');
-      inputs[2].value = localStorage.getItem('msg');
-    }
-  }
-  else {
-    // Too bad, no localStorage for us
-  }  
 }
 
 function saveUserData() {
@@ -45,5 +29,20 @@ function saveUserData() {
   localStorage.setItem('email', inputs[1].value);
   localStorage.setItem('msg', inputs[2].value);
 }
-inputs.forEach(input => input.addEventListener('input', saveUserData));
+
+function loadUserData() {
+  if (storageAvailable('localStorage')) { // Check if storage is abailable
+    if (localStorage.length === 0) {
+      saveUserData();
+    } else {
+      inputs[0].value = localStorage.getItem('name');
+      inputs[1].value = localStorage.getItem('email');
+      inputs[2].value = localStorage.getItem('msg');
+    }
+  } else {
+    // Too bad, no localStorage for us
+  }
+}
+
+inputs.forEach((input) => input.addEventListener('input', saveUserData));
 loadUserData();
